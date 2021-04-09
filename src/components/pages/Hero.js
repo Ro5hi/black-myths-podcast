5ab865539c43d0ff219946ca8475214d14e5630a
Ro5hi <slen.detablan@gmail.com>
2021-04-09 16:48:35 -0600

    added img logo for hero section

---
+++ src/components/pages/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return ( 
+        <div>
+            <img src="/images/bmp.png" alt="logo"></img>
+        </div>
+    )
+}
+
+export default Hero;
