2b30db749c14a628aa8532aeceecb13c163ff04a
Ro5hi <slen.detablan@gmail.com>
2021-04-09 16:55:20 -0600

    added basic about structure, added info about myths page

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about">
+            <p>
+                An Informative, conversational show analyzing popular myths about Black culture from a sociopolitical and cultural nature.<br/>
+                TLDR: "We just be debunking the bullshit that be said about Black people."
+            </p>
+        </div>
+    )
+}
+
+export default About;
--- src/components/pages/Info.js
+++ src/components/pages/Info.js
@@ -3,7 +3,25 @@ import React from 'react';
 const Info = () => {
     return (
         <div className="myth">
-            
+            <strong>What is a myth?</strong>
+            <p>
+                An unfounded or false notion. Example: Racial superiority.
+            </p>
+            <div className="first-episode">
+                <strong>First Episode</strong>
+                <p>
+                    Done amongst the podcast members only. Usually lightt-hearted with a lot of jokes, highly informative, and fun.
+                </p>
+            </div>
+            <div className="second-episode">
+                <strong>Second Episode</strong>
+                <p>
+                    An interview with an expert. It is more serious, in-depth, able to frame 
+                    the myth in ways we never thought of, and adds more context to the myth.
+                </p>
+            </div>
         </div>
     )
 }
+
+export default Info;
