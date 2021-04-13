72417a9c43228eb79d7dea6360f784e789093a69
Ro5hi <slen.detablan@gmail.com>
2021-04-13 11:29:16 -0600

    added page styles file for CSS, using styled components

---
+++ src/components/pages/PageStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const About = ``
+
+export const Featured = ``
+
+export const Hero = ``
+
+export const Footer = ``
+
+export const Info = ``
+
+export const Home = ``
+
+export const Team = ``
