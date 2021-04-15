e8209b2c79d6a8c6c9233fc8226706f1cc4a4441
Ro5hi <slen.detablan@gmail.com>
2021-04-15 14:56:25 -0600

    globalinject font-family for styles

--- src/components/pages/PageStyles.js
+++ src/components/pages/PageStyles.js
@@ -1,4 +1,12 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+    @font-face {
+    font-family: "Montserrat", sans-serif;
+    src: url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap');
+    }
+`
 
 export const About = ``
 
