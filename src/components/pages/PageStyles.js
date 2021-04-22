446f0101636e9427842c150efd1b7a9ca2931edd
Ro5hi <slen.detablan@gmail.com>
2021-04-22 15:45:35 -0600

    imported ont family for app, removed code from pagestyles

--- src/components/pages/PageStyles.js
+++ src/components/pages/PageStyles.js
@@ -1,13 +1,6 @@
 import styled from 'styled-components';
 import { injectGlobal } from 'styled-components';
 
-injectGlobal`
-    @font-face {
-    font-family: "Montserrat", sans-serif;
-    src: url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap');
-    }
-`
-
 export const About = ``
 
 export const Featured = ``
