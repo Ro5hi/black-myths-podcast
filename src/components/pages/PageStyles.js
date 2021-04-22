b362a67fd6a8c50efe751ef94d85b058aad6d8b8
Ro5hi <slen.detablan@gmail.com>
2021-04-22 15:49:04 -0600

    imported App.css file, added comments

--- src/components/pages/PageStyles.js
+++ src/components/pages/PageStyles.js
@@ -1,16 +1,30 @@
 import styled from 'styled-components';
 import { injectGlobal } from 'styled-components';
 
+
+// About 
 export const About = ``
 
+
+// Featured 
 export const Featured = ``
 
+
+// Hero 
 export const Hero = ``
 
+
+//Footer
 export const Footer = ``
 
+
+// Info
 export const Info = ``
 
+
+// Home 
 export const Home = ``
 
+
+//Team
 export const Team = ``
