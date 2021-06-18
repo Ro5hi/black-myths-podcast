31287819d50b207fb15664956d30604d61a4ff72
Ro5hi <slen.detablan@gmail.com>
2021-06-18 12:20:51 -0600

    added fragment to Team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 const Team = () => {
     return (
-        <div className="team" id="team">
-
-        </div>
+        <>
+        </>
     )
 };
-
-// need bio from team members and respective profile pictures
 
 export default Team;
