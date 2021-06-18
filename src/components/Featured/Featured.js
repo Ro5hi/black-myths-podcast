86c40897bbea42ced31a800631690f31b83f7959
Ro5hi <slen.detablan@gmail.com>
2021-06-18 12:20:29 -0600

    added fragment to featured

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 const Featured = () => {
     return (
-        <div className="featured" id="featured">
-            
-        </div>
+        <>
+        </>
     )
 };
-
-// need image files from recent guests
 
 export default Featured;
