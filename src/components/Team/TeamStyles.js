261a732021a3f72cfcc88ef1da41bc029a82bd8b
Ro5hi <slen.detablan@gmail.com>
2021-06-19 12:15:49 -0600

    updated team, working on Team styles, added imgs

--- src/components/Team/TeamStyles.js
+++ src/components/Team/TeamStyles.js
@@ -1,19 +1,80 @@
 import styled from "styled-components";
 
 export const TeamContainer = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #ffffff;
+
+    @media screen and (max-width: 768px) {
+        height: 1100px;
+    }
+
+    @media screen and (max-width: 480px) {
+        height: 1200px;
+    }
 `
 
 export const TeamWrapper = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    padding: 0 15px;
+    grid-gap: 20px;
+
+    @media screen and (max-width: 1000px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+        padding: 0 20px;
+    }
 `
 
 export const TeamCard = styled.div`
+    background: black;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column:
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 10px;
+    max-height: 400px;
+    max-width: 400px;
+    padding: 15px;
+    
+    &:hover {
+        transform: scale(1.05);
+        transition: all 0.2 ease-in-out;
+        cursor: pointer;
+    }
 `
 
 export const MemberPhoto = styled.div`
+    max-height: 100%;
+    max-width: 100%;
+    margin-top: 14px;
+    margin-bottom: 14px;
 `
 
 export const MemberName = styled.div`
+    color: red;
+    font-size: 20px;
+    text-align: center;
 `
 
 export const MemberHeadline = styled.div`
+    color: white;
+    font-size: 16px;
+    text-align: left;
+    
+    @media screen and (max-width: 480px) {
+        font-size: 12px;
+      }
 `
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -7,34 +7,39 @@ import { TeamContainer,
          MemberHeadline 
 } from './TeamStyles';
 
-const Team = ({ member1, headline1, member2, headline2, member3, headline3, member4, headline4, member5, headline5 }) => {
+const Team = ({ member1, headline1, member2, headline2, member3, headline3, member4, headline4, member5, headline5, member6, headline6 }) => {
     return (
         <TeamContainer id="team">
             <TeamWrapper>
                 <TeamCard>
-                    <MemberPhoto>  </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\1.png" alt="tooblack"/> </MemberPhoto>
                     <MemberName> {member1} </MemberName>
                     <MemberHeadline> {headline1} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto> </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\2.png" alt="elle"/> </MemberPhoto>
                     <MemberName> {member2} </MemberName>
                     <MemberHeadline> {headline2} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto>  </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\3.png" alt="ryan"/> </MemberPhoto>
                     <MemberName> {member3} </MemberName>
                     <MemberHeadline> {headline3} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto>  </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\4.png" alt="shelle"/> </MemberPhoto>
                     <MemberName> {member4} </MemberName>
                     <MemberHeadline> {headline4} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto>  </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\5.png" alt="tyrell"/> </MemberPhoto>
                     <MemberName> {member5} </MemberName>
                     <MemberHeadline> {headline5} </MemberHeadline>
+                </TeamCard>
+                <TeamCard>
+                    <MemberPhoto> <img src="\images\team\6.png" alt="kameron"/> </MemberPhoto>
+                    <MemberName> {member6} </MemberName>
+                    <MemberHeadline> {headline6} </MemberHeadline>
                 </TeamCard>
             </TeamWrapper>
         </TeamContainer>
