4cc237f72fd4934c78bf621b332935196219f387
Ro5hi <slen.detablan@gmail.com>
2021-06-19 11:17:46 -0600

    updated Team, added data to Team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 const Team = () => {
     return (
-        <>
-        </>
+        <TeamContainer>
+            <TeamWrapper>
+                <TeamCard>
+                    <MemberPhoto>  </MemberPhoto>
+                    <MemberName>  </MemberName>
+                    <MemberHeadline>  </MemberHeadline>
+                </TeamCard>
+                <TeamCard>
+                    <MemberPhoto>  </MemberPhoto>
+                    <MemberName>  </MemberName>
+                    <MemberHeadline>  </MemberHeadline>
+                </TeamCard>
+                <TeamCard>
+                    <MemberPhoto>  </MemberPhoto>
+                    <MemberName>  </MemberName>
+                    <MemberHeadline>  </MemberHeadline>
+                </TeamCard>
+                <TeamCard>
+                    <MemberPhoto>  </MemberPhoto>
+                    <MemberName>  </MemberName>
+                    <MemberHeadline>  </MemberHeadline>
+                </TeamCard>
+                <TeamCard>
+                    <MemberPhoto>  </MemberPhoto>
+                    <MemberName>  </MemberName>
+                    <MemberHeadline>  </MemberHeadline>
+                </TeamCard>
+            </TeamWrapper>
+        </TeamContainer>
     )
 };
 
---
+++ src/components/Team/TeamData.js
@@ -0,0 +1,14 @@
+const TeamData = {
+    member1: 'Too Black',
+    headline1: 'Founder. Spoken poet, educator, and activist.',
+    member2: 'Elle Black',
+    headline: 'Writer, artist, and facilitator based in Indianapolis.',
+    member3: 'Ryan J. Mills',
+    headline: 'Producer. Writer, artist, and volunteer.',
+    member4: 'Shelle Terrell',
+    headline4: 'Body adorner, entrepreneur, business consultant, and designated comedian.',
+    member5: 'Terell L. Alexander',
+    headline5: 'Telecommunications Major. Phi Beta Sigma & Alpha Phi Omega National Service Fraternity.',
+    member6: '',
+    headline6: '',
+}
