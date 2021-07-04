cc50bfd596fd17ad0448ce02f272ef430bc5dc12
Ro5hi <slen.detablan@gmail.com>
2021-07-03 18:34:02 -0600

    updated Contact component and styles

---
+++ src/components/Contact/ContactStyles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ContactContainer = styled.div``
+
+export const ContactWrapper = styled.div``
+
+export const ContactInfo = styled.div``
+
+export const ContactInput = style.div`` 
---
+++ src/components/Contact/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// 
+
+const Contact = () => {
+    return (
+        <ContactContainer>
+            <ContactWrapper>
+                <ContactInfo>
+                    <ContactInput>
+
+                    </ContactInput>
+                </ContactInfo>
+            </ContactWrapper>
+        </ContactContainer>
+    )
+}
+
+export default Contact
