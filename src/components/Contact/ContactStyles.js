318983bad55369a579ba7d6cfa8a77529cee75d3
Ro5hi <slen.detablan@gmail.com>
2021-07-03 18:40:50 -0600

    updated styles

--- src/components/Contact/ContactStyles.js
+++ src/components/Contact/ContactStyles.js
@@ -1,9 +1,49 @@
 import styled from "styled-components";
 
-export const ContactContainer = styled.div``
+export const ContactContainer = styled.div`
+    display: flex;
+    background: black;
+    height: 800px;
+    justify-content: center;
+    align-items: center;
 
-export const ContactWrapper = styled.div``
+    @media screen and (max-width: 768px) {
+        height: 900px;
+    }
 
-export const ContactInfo = styled.div``
+    @media screen and (max-width: 480px) {
+        height: 860px;
+    }
+`
 
-export const ContactInput = style.div`` 
+export const ContactWrapper = styled.div`
+    max-width: 700px;
+    height: 860px;
+    justify-content: center;
+`
+
+export const ContactInfo = styled.div`
+    align-items: center;
+    color: white;
+    font-size: 16px;
+    line-height: 18px;
+    letter-spacing: 1.4px;
+    text-transform: uppercase;
+    margin-bottom: 24px;
+`
+
+export const ContactInput = style.div`
+    display: flex;
+    justify-content: flex-start;
+    max-width: 400px;
+    max-height: 400px;
+`
+
+export const SendBtn = styled.div`
+    display: flex;
+    align-items: center;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`
