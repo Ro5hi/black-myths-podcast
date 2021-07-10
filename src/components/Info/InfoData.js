7cf973d833e3df3edded6b6fbb19c6809793f650
Ro5hi <slen.detablan@gmail.com>
2021-07-10 13:40:21 -0600

    fixed typo

--- src/components/Info/InfoData.js
+++ src/components/Info/InfoData.js
@@ -3,7 +3,7 @@ export const InfoData = {
     header1: 'What is a myth?',
     desc: 'An unfounded or false notion. Example: Racial superiority.',
     header2: 'First Episode',
-    ep2: 'Done amongst the podcast members only. Usually lightt-hearted with a lot of jokes, highly informative, and fun.',
+    ep2: 'Done amongst the podcast members only. Usually light-hearted with a lot of jokes, highly informative, and fun.',
     header3: 'Second Episode',
     ep3: 'An interview with an expert. It is more serious, in-depth, able to frame the myth in ways we never thought of, and adds more context to the myth.'
 }
