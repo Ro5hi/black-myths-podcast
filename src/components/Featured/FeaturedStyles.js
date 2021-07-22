0487238fbb026e87198b56d90d3d483fcc8a8e9b
Ro5hi <slen.detablan@gmail.com>
2021-07-22 16:14:10 -0600

    updated styles and component

--- src/components/Featured/FeaturedStyles.js
+++ src/components/Featured/FeaturedStyles.js
@@ -3,3 +3,14 @@ import styled from 'styled-components'
 export const FeaturedContainer = styled.div`
 
 `
+export const FeaturedWrapper = styled.div``
+
+export const FeaturedHeadline = styled.div``
+
+export const FeaturedGuests = styled.div``
+
+export const Featured1 = styled.div``
+
+export const Featured2 = styled.div``
+
+export const Featured3 = styled.div``
--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -5,9 +5,11 @@ const Featured = () => {
         <FeaturedContainer>
         <FeaturedWrapper>
             <FeaturedHeadline>
-                <Featured1> </Featured1>
-                <Featured2> </Featured2>
-                <Featured3>  </Featured3>
+                <FeaturedGuests>
+                    <Featured1> </Featured1>
+                    <Featured2> </Featured2>
+                    <Featured3>  </Featured3>
+                </FeaturedGuests>
             </FeaturedHeadline>
         </FeaturedWrapper>
     </FeaturedContainer>
