07d6df5148d1456e4b5502ad5a6237e95857ad3e
Ro5hi <slen.detablan@gmail.com>
2021-07-22 16:12:13 -0600

    updated Featured component

---
+++ src/components/Featured/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Featured = () => {
+    return (
+        <FeaturedContainer>
+        <FeaturedWrapper>
+            <FeaturedHeadline>
+                <Featured1> </Featured1>
+                <Featured2> </Featured2>
+                <Featured3>  </Featured3>
+            </FeaturedHeadline>
+        </FeaturedWrapper>
+    </FeaturedContainer>
+    )
+};
+
+export default Featured;
