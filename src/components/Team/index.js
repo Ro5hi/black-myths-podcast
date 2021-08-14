e62819471eab1b6098c079ec5e2fbaeee69466e9
Ro5hi <slen.detablan@gmail.com>
2021-08-13 19:39:47 -0600

    changed octals to string

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -12,32 +12,32 @@ const Team = ({ member1, headline1, member2, headline2, member3, headline3, memb
         <TeamContainer id="team">
             <TeamWrapper>
                 <TeamCard>
-                    <MemberPhoto> <img src="\images\team\1.png" alt="tooblack"/> </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\tb.png" alt="tooblack"/> </MemberPhoto>
                     <MemberName> {member1} </MemberName>
                     <MemberHeadline> {headline1} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto> <img src="\images\team\2.png" alt="elle"/> </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\elle.png" alt="elle"/> </MemberPhoto>
                     <MemberName> {member2} </MemberName>
                     <MemberHeadline> {headline2} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto> <img src="\images\team\3.png" alt="ryan"/> </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\ryan.png" alt="ryan"/> </MemberPhoto>
                     <MemberName> {member3} </MemberName>
                     <MemberHeadline> {headline3} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto> <img src="\images\team\4.png" alt="shelle"/> </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\shelle.png" alt="shelle"/> </MemberPhoto>
                     <MemberName> {member4} </MemberName>
                     <MemberHeadline> {headline4} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto> <img src="\images\team\5.png" alt="tyrell"/> </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\ty.png" alt="tyrell"/> </MemberPhoto>
                     <MemberName> {member5} </MemberName>
                     <MemberHeadline> {headline5} </MemberHeadline>
                 </TeamCard>
                 <TeamCard>
-                    <MemberPhoto> <img src="\images\team\6.png" alt="kameron"/> </MemberPhoto>
+                    <MemberPhoto> <img src="\images\team\kam.png" alt="kameron"/> </MemberPhoto>
                     <MemberName> {member6} </MemberName>
                     <MemberHeadline> {headline6} </MemberHeadline>
                 </TeamCard>
