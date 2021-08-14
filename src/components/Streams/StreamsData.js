f81dfefe717cad528bcf8d3bda97ca4c77d52c47
Ro5hi <slen.detablan@gmail.com>
2021-08-13 19:25:39 -0600

    debugged error to deploy data

--- src/components/Streams/StreamsData.js
+++ src/components/Streams/StreamsData.js
@@ -1,4 +1,4 @@
-const StreamsData = {
+export const StreamsData = {
     Spotify: "https://open.spotify.com/show/70RdUs6GHCWmOlFvceGglf",
     Youtube: "https://www.youtube.com/watch?v=0UM-Xm2VB50",
     Libsyn: "https://blackmyths.libsyn.com"
