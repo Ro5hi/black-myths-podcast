47005d8815478b0da3dde6341ac18c947ae31c88
Ro5hi <slen.detablan@gmail.com>
2021-08-19 17:39:45 -0600

    updated index.js for featured, removed unused styles

--- src/components/Featured/FeaturedStyles.js
+++ src/components/Featured/FeaturedStyles.js
@@ -30,12 +30,8 @@ export const FeaturedWrapper = styled.div`
     }
 `
 
+
+
+
 export const FeaturedHeadline = styled.div``
 
-export const FeaturedGuests = styled.div``
-
-export const Featured1 = styled.div``
-
-export const Featured2 = styled.div``
-
-export const Featured3 = styled.div``
