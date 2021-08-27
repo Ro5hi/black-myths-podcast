89b32a8803523f2c808de65bc5a4ef75b88cc88b
Ro5hi <slen.detablan@gmail.com>
2021-08-27 17:02:52 -0600

    added data to card in index

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { FeaturedContainer, FeaturedWrapper, FeaturedCard, FeaturedName } from './FeaturedStyles';
+import { FeaturedContainer, FeaturedWrapper, FeaturedCard, FeaturedName, FeaturedPhoto } from './FeaturedStyles';
 
-const Featured = (Guest1Name, Guest1Photo, Guest2Name, Guest2Photo, Guest3Name, Guest3Photo) => {
+const Featured = ({ Guest1Name, Guest1Photo, Guest2Name, Guest2Photo, Guest3Name, Guest3Photo }) => {
     return (
         <FeaturedContainer>
         <FeaturedWrapper>
             <FeaturedCard>
+                <FeaturedPhoto> </FeaturedPhoto>
                 <FeaturedName> {Guest1Name} </FeaturedName>
             </FeaturedCard>
         </FeaturedWrapper>
