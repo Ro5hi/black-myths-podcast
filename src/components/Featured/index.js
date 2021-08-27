2a276717e69c92a92cb529f27f9e17a06626f31e
Ro5hi <slen.detablan@gmail.com>
2021-08-27 17:09:59 -0600

    rendered first guest

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -6,7 +6,15 @@ const Featured = ({ Guest1Name, Guest1Photo, Guest2Name, Guest2Photo, Guest3Name
         <FeaturedContainer id="featured">
         <FeaturedWrapper>
             <FeaturedCard>
-                <FeaturedPhoto> </FeaturedPhoto>
+                <FeaturedPhoto> <img src="\images\guests\drjj.jpg" alt="JoyJames" /> </FeaturedPhoto>
+                <FeaturedName> {Guest1Name} </FeaturedName>
+            </FeaturedCard>
+            <FeaturedCard>
+                <FeaturedPhoto> <img src="\images\guests\drjj.jpg" alt="JoyJames" /> </FeaturedPhoto>
+                <FeaturedName> {Guest1Name} </FeaturedName>
+            </FeaturedCard>
+            <FeaturedCard>
+                <FeaturedPhoto> <img src="\images\guests\drjj.jpg" alt="JoyJames" /> </FeaturedPhoto>
                 <FeaturedName> {Guest1Name} </FeaturedName>
             </FeaturedCard>
         </FeaturedWrapper>
