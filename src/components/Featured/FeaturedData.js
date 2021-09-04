1178d3c8516eea5de67110477236bc41ac3fdda0
Ro5hi <slen.detablan@gmail.com>
2021-09-03 18:39:05 -0600

    added card 3 and img

--- src/components/Featured/FeaturedData.js
+++ src/components/Featured/FeaturedData.js
@@ -3,6 +3,6 @@ const FeaturedData = {
     Guest1Photo= "",
     Guest2Name = "Glen Ford",
     Guest2Photo= "",
-    Guest3Name = "Dr. Rasul Mowatt",
+    Guest3Name = "Rasul Mowatt",
     Guest3Photo = ""
 }
--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -10,8 +10,12 @@ const Featured = ({ Guest1Name, Guest1Photo, Guest2Name, Guest2Photo, Guest3Name
                 <FeaturedName> {Guest1Name} </FeaturedName>
             </FeaturedCard>
             <FeaturedCard>
-                <FeaturedPhoto> <img src="\images\guests\glenf.jpg" alt="JoyJames" /> </FeaturedPhoto>
+                <FeaturedPhoto> <img src="\images\guests\glenf.jpg" alt="GlenFord" /> </FeaturedPhoto>
                 <FeaturedName> {Guest2Name} </FeaturedName>
+            </FeaturedCard>
+            <FeaturedCard>
+                <FeaturedPhoto> <img src="\images\guests\r_m.png" alt="RasulMowatt" /> </FeaturedPhoto>
+                <FeaturedName> {Guest3Name} </FeaturedName>
             </FeaturedCard>
         </FeaturedWrapper>
     </FeaturedContainer>
